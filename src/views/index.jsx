a0a7638951242b714ba13971540652cd71b18b9c
Josh Ruihley <ruihley@gmail.com>
2017-09-10 20:29:01 -0400

    static render

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -5,20 +5,30 @@ import style from './style.scss'
 export default class TemplateModule extends React.Component {
 
   render() {
-    return <PollResult />
+    return <PollResults />
   }
 }
 
+class PollResults extends React.Component {
+  render () {
+    return (
+      <div>
+        <PollResult hashtag='#music' />
+        <PollResult hashtag='#favoriteplayer' />
+      </div>
+    )
+  }
+}
 
 class PollResult extends React.Component {
     render () {
         return (
             <div>
-                <h4>#hashtag</h4>
+                <h4>{this.props.hashtag}</h4>
                 <ul>
-                    <PollOption id='1'/>
-                    <PollOption id='2'/>
-                    <PollOption id='3'/>
+                    <PollOption id='1' count='43'/>
+                    <PollOption id='2' count='12'/>
+                    <PollOption id='3' count='10'/>
                 </ul>
             </div>
         )
@@ -30,7 +40,7 @@ class PollOption extends React.Component {
         return (
             <li id="option{this.props.id}">
                 <div id="option{this.props.id}name">Name</div>
-                <div id="option{this.props.id}value">0</div>
+                <div id="option{this.props.id}value">{this.props.count}</div>
             </li>
         )
     }
